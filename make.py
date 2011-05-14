47c23756db233190bb8d813641dbc0572d9d0829
Marc Culler <culler@users.noreply.github.com>
2011-05-14 10:03:51 -0500

    sets platform specific variables when calling make.

---
+++ make.py
@@ -0,0 +1,23 @@
+from os import environ
+from sys import platform
+from subprocess import call
+
+if platform == 'Darwin':
+    args = [
+        'make',
+        'GNATMAKE=/usr/local/ada-4.3/bin/gnatmake',
+        'GNATBIND=/usr/local/ada-4.3/bin/gnatbind',
+        ('GNATFLAGS=-gnat95 -gnatv -O3 -gnatp -gnatf '+
+         '--GNATBIND="gnatbind -static"')
+        ]
+elif platform == 'linux2':
+    args = [
+        'make',
+        'GNATMAKE=gnatmake',
+        'GNATBIND=gnatbind',
+        ('GNATFLAGS=-gnat95 -gnatv -O3 -gnatp -gnatf -fPIC '+
+         '--GNATBIND="gnatbind -static"')
+        ]
+
+call(args)
+    
