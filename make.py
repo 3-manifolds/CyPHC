a74764a036793288c06e75a387e26d7746754e9e
Marc Culler <culler@users.noreply.github.com>
2011-05-18 12:25:22 -0500

    make stuff

--- make.py
+++ make.py
@@ -1,23 +1,32 @@
 from os import environ
 from sys import platform
-from subprocess import call
+from subprocess import Popen, PIPE, call
 
 if platform == 'darwin':
+    gnatmake, errors = Popen(['which', 'gnatmake'],
+                             stdin=PIPE, stderr=PIPE).communicate()
+    gnatmake = gnatmake.strip()
+    if not gnatmake:
+        gnatmake = '/usr/local/ada-4.3/bin/gnatmake'
+    gnatlink, errors = Popen(['which', 'gnatlink'],
+                             stdin=PIPE, stderr=PIPE).communicate()
+    gnatlink = gnatlink.strip()
+    if not gnatlink:
+        gnatlik = '/usr/local/ada-4.3/bin/gnatlink'
+        
     args = [
         'make',
-        'GNATMAKE=/usr/local/ada-4.3/bin/gnatmake',
-        'GNATBIND=/usr/local/ada-4.3/bin/gnatbind',
-        ('GNATFLAGS=-gnat95 -gnatv -O3 -gnatp -gnatf '+
-         '--GNATBIND="gnatbind -static"')
+        'GNATMAKE=%s'%gnatlink,
+        'GNATBIND=%s'%gnatbind,
+        'GNATFLAGS=-gnat95 -gnatv -O3 -gnatp -gnatf '
         ]
 elif platform == 'linux2':
     args = [
         'make',
         'GNATMAKE=gnatmake',
         'GNATBIND=gnatbind',
-        ('GNATFLAGS=-gnat95 -gnatv -O3 -gnatp -gnatf -fPIC '+
-         '--GNATBIND="gnatbind -static"'),
-        'FLAGS=-fPIC' 
+        'GNATFLAGS=-gnat95 -gnatv -O3 -gnatp -gnatf -fPIC ',
+        'GCCFLAGS=-fPIC' 
         ]
 else:
     print 'Unknown platform'
