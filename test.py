9ab473911d34d5d95d983ff4d38a966b673ac6c9
Nathan Dunfield <nathan@dunfield.info>
2011-07-29 14:16:58 -0500

    Brought test.py up to date

--- test.py
+++ test.py
@@ -4,10 +4,5 @@ p1 = PHCPoly(R, '1 + 2*x + 3*x*y + 4*y')
 p2 = PHCPoly(R, '5 + 6*x^2*y + 7*x*y^2')
 S = PHCSystem(R, [p1, p2])
 print S
-sys, sol = S.MVstart()
-print "start system"
-print sys
-print "start solution"
-print sol
-print "solution list"
+S.MVsolve()
 print S.solution_list()
