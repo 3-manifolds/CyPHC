a31d4bfbf602911a30d827c5d5d2eb763f8864d5
Marc Culler <Marc Culler@localhost>
2017-11-07 08:54:04 -0600

    Reverting the attempt to preserve sigint handlers, because it breaks interrupt handling altogether.

--- cyphc_src/__init__.py
+++ cyphc_src/__init__.py
@@ -1,10 +1,6 @@
-import signal
-handler = signal.getsignal(signal.SIGINT)
 from .phc import *
 try:
     import cysignals
-    _ = cysignals.init_cysignals()
+    cysignals.init_cysignals()
 except ImportError:
     pass
-signal.signal(signal.SIGINT, handler)
-
