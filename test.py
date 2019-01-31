a7446be6915451422d516fb99cbafe08ea3f4486
Nathan Dunfield <nathan@dunfield.info>
2019-01-31 13:29:18 -0600

    Py3 support: fixed print statements

--- test.py
+++ test.py
@@ -3,6 +3,6 @@ R = PolyRing(['x','y'])
 p1 = PHCPoly(R, '1 + 2*x + 3*x*y + 4*y')
 p2 = PHCPoly(R, '5 + 6*x^2*y + 7*x*y^2')
 S = PHCSystem(R, [p1, p2])
-print S
+print(S)
 S.MVsolve()
-print S.solution_list()
+print(S.solution_list())
