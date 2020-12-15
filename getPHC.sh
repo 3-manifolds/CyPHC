2d8389f47c7df6f5887b8885d7c0435d6c93ecaa
Nathan Dunfield <nathan@dunfield.info>
2020-12-15 08:34:20 -0600

    Use newest PHCpack so compiles with gcc 9.3 on Ubuntu 20.04

--- getPHC.sh
+++ getPHC.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-PHCDIR=PHCpack-2.4.47
-PHCTAR=v2.4.47.tar.gz
+PHCDIR=PHCpack-2.4.82
+PHCTAR=v2.4.82.tar.gz
 PHCURL=https://github.com/janverschelde/PHCpack/archive/$PHCTAR
 curl -L -O $PHCURL 
 tar xvfz $PHCTAR
